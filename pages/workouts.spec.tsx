92c97d11f2fc38b02067e5069fb881a7361042fc
Krystian Kat <krystian.kat@protonmail.com>
2022-04-18 11:31:36 +0200

    test: create workouts spec file

---
+++ pages/workouts.spec.tsx
@@ -0,0 +1,57 @@
+import React from 'react';
+import { act, render, RenderResult } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { expect } from '@jest/globals';
+import Workouts, { MotivationSentenceContext } from './workouts';
+import '@testing-library/jest-dom';
+import { MotivationSentence } from '../components/MotivationSentence';
+
+describe(MotivationSentence.name, () => {
+  let context: RenderResult<
+    typeof import('../node_modules/@testing-library/dom/types/queries'),
+    HTMLElement,
+    HTMLElement
+  >;
+
+  beforeEach(() => {
+    jest.useFakeTimers();
+
+    act(() => {
+      context = render(
+        <MotivationSentenceTestProvider>
+          <MotivationSentence />
+        </MotivationSentenceTestProvider>
+      );
+    });
+  });
+
+  afterEach(() => {
+    jest.runOnlyPendingTimers();
+    jest.useRealTimers();
+  });
+
+  it('should render motivation sentence after 10 seconds', async () => {
+    const defaultSentence = await context.findByText(
+      'Training is the key to success'
+    );
+    expect(defaultSentence).not.toBeNull();
+
+    act(() => {
+      jest.advanceTimersByTime(10_000);
+    });
+
+    expect(await context.findByText('code().then().code()')).not.toBeNull();
+  });
+});
+
+const MotivationSentenceTestProvider: React.FC = ({ children }) => {
+  return (
+    <MotivationSentenceContext.Provider
+      value={{
+        getMotivationSentence: async () => 'code().then().code()',
+      }}
+    >
+      {children}
+    </MotivationSentenceContext.Provider>
+  );
+};
