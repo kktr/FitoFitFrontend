9fbc1847022439eecaf0aee0960d9bdb233f2a25
Krystian Kat <krystian.kat@protonmail.com>
2022-04-18 22:20:12 +0200

    feat: add images domain url

--- next.config.js
+++ next.config.js
@@ -2,6 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   excludeFile: (str) => /\*.{spec,test}.js/.test(str),
+  images: { domains: ['cryptic-gorge-42821.herokuapp.com'] },
 };
 
 module.exports = nextConfig;
