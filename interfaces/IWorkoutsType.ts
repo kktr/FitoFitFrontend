1f3835ef8e1a73a631a840d717f523e863a54ddd
Krystian Kat <krystian.kat@protonmail.com>
2022-04-18 20:25:30 +0200

    refactor: delete empty file

--- interfaces/IWorkoutsType.ts
+++
@@ -1,8 +0,0 @@
-// export type IWorkoutsType = 'Cardio' | 'Cycling' | 'Running' | 'General';
-
-// export const workoutsTypes: IWorkoutsType[] = [
-//   'General',
-//   'Cardio',
-//   'Cycling',
-//   'Running',
-// ];
